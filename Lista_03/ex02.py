c0cafe6a3e6bf3b1fade1792b4cf1277b7891c19
Gilbert-Silva <gilbertif1995@outlook.com>
2024-03-01 17:41:03 -0300

    Lista 03

---
+++ Lista_03/ex02.py
@@ -0,0 +1,17 @@
+nome = input("Digite seu nome completo: ")
+p = nome.split()
+print("Bem-vindo(a) ao Python,", p[0])
+
+"""
+Gilbert Azevedo da Silva
+"""
+
+k = nome.index(" ")
+primeiro_nome = nome[0:k]
+print("Bem-vindo(a) ao Python,", primeiro_nome)
+
+
+
+
+
+
