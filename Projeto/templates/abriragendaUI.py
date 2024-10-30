f421b40f6ff3bc3cd08dcc4fb6335c934226b9f4
Gilbert-Silva <gilbertif1995@outlook.com>
2024-10-30 14:48:18 +0000

    Abrir agenda

--- Projeto/templates/abriragendaUI.py
+++ Projeto/templates/abriragendaUI.py
@@ -5,3 +5,8 @@ import time
 class AbrirAgendaUI:
     def main():
         st.header("Abrir Agenda do Dia")
+        data = "30/10/2024"
+        inicio = "08:00"
+        fim = "12:00"
+        intervalo = 30
+        View.horario_abrir_agenda(data, inicio, fim, intervalo)
--- Projeto/views.py
+++ Projeto/views.py
@@ -1,6 +1,7 @@
 from models.cliente import Cliente, Clientes
 from models.horario import Horario, Horarios
 from models.servico import Servico, Servicos
+from datetime import datetime, timedelta
 
 class View:
     def cliente_inserir(nome, email, fone):
@@ -38,6 +39,34 @@ class View:
     def horario_excluir(id):
         c = Horario(id, None)
         Horarios.excluir(c)    
+
+    def horario_abrir_agenda(data, hora_inicio, hora_fim, intervalo):
+        #data = "30/10/2024"
+        #inicio = "08:00"
+        #fim = "12:00"
+        #intervalo = 30
+        i = data + " " + hora_inicio   # "30/10/2024 08:00"
+        f = data + " " + hora_fim      # "30/10/2024 12:00"
+        d = timedelta(minutes=intervalo)
+        di = datetime.strptime(i, "%d/%m/%Y %H:%M")
+        df = datetime.strptime(f, "%d/%m/%Y %H:%M")
+        x = di
+        while x <= df:
+            #cadastrar o horário x
+            View.horario_inserir(x, False, None, None)
+            #passar para o próximo horário
+            x = x + d
+        """
+        h = "30/10/2024 08:00"
+        h = "30/10/2024 08:30"
+        h = "30/10/2024 09:00"
+        h = "30/10/2024 09:30"
+        h = "30/10/2024 10:00"
+        h = "30/10/2024 10:30"
+        h = "30/10/2024 11:00"
+        h = "30/10/2024 11:30"
+        h = "30/10/2024 12:00"
+        """
 
     def servico_inserir(descricao, valor, duracao):
         c = Servico(0, descricao, valor, duracao)
