093f08ddd2ca40334e35cfaec6b589d3e2f82ccf
Gilbert-Silva <gilbertif1995@outlook.com>
2024-11-06 12:01:15 -0300

    admin

--- Projeto/index.py
+++ Projeto/index.py
@@ -4,19 +4,44 @@ from templates.manterservicoUI import ManterServicoUI
 from templates.abriragendaUI import AbrirAgendaUI
 from templates.abrircontaUI import AbrirContaUI
 from templates.loginUI import LoginUI
+from views import View
 
 import streamlit as st
 
 class IndexUI:
     def main():
+        View.cliente_admin()
         if "cliente_id" in st.session_state:
             st.sidebar.write("Bem-vindo(a), " + st.session_state["cliente_nome"])
-        op = st.sidebar.selectbox("Menu", ["Cadastro de Clientes", "Cadastro de Horários", "Cadastro de Serviços", "Abrir Agenda do Dia", "Abrir Conta", "Entrar no Sistema"])
+            if st.session_state["cliente_nome"] == "admin":
+                IndexUI.menu_admin()
+            else:
+                IndexUI.menu_cliente()
+        else:
+            IndexUI.menu_visitante()
+
+    def menu_visitante():
+        op = st.sidebar.selectbox("Menu", ["Entrar no Sistema", "Abrir Conta"])
+        if op == "Abrir Conta": AbrirContaUI.main()
+        if op == "Entrar no Sistema": LoginUI.main()
+
+    def menu_cliente():
+        pass
+
+    def menu_admin():            
+        op = st.sidebar.selectbox("Menu", ["Cadastro de Clientes", "Cadastro de Horários", "Cadastro de Serviços", "Abrir Agenda do Dia"])
         if op == "Cadastro de Clientes": ManterClienteUI.main()
         if op == "Cadastro de Horários": ManterHorarioUI.main()
         if op == "Cadastro de Serviços": ManterServicoUI.main()
         if op == "Abrir Agenda do Dia": AbrirAgendaUI.main()
-        if op == "Abrir Conta": AbrirContaUI.main()
-        if op == "Entrar no Sistema": LoginUI.main()
+        IndexUI.sair_do_sistema()
+    
+    def sair_do_sistema():
+        if st.sidebar.button("Sair"):
+            del st.session_state["cliente_id"]
+            del st.session_state["cliente_nome"]
+            st.rerun()
+
+
         
 IndexUI.main()
--- Projeto/views.py
+++ Projeto/views.py
@@ -4,6 +4,11 @@ from models.servico import Servico, Servicos
 from datetime import datetime, timedelta
 
 class View:
+    def cliente_admin():
+        for c in View.cliente_listar():
+            if c.email == "admin": return
+        View.cliente_inserir("admin", "admin", "1234", "1234")
+
     def cliente_inserir(nome, email, fone, senha):
         c = Cliente(0, nome, email, fone, senha)
         Clientes.inserir(c)
@@ -19,7 +24,7 @@ class View:
         Clientes.atualizar(c)
 
     def cliente_excluir(id):
-        c = Cliente(id, "", "", "")
+        c = Cliente(id, "", "", "", "")
         Clientes.excluir(c)    
 
     def cliente_autenticar(email, senha):
